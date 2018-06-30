f6921c87225baa99abf46d923c819f26fc8231cc
Fernando Sutter <fernando.sutter1@gmail.com>
2018-06-30 15:53:58 -0300

    Compilado os arquivos.

--- src/Impressora.java
+++ src/Impressora.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Impressora extends Processo {
-	public static void main(String []args) {
+	public void main(String []args) {
 	System.out.println("Informe o quantum: ");
 	Scanner s = new Scanner (System.in);
-	quamtum = s.nextInt();
+	quantum = s.nextInt();
 	}
  
 }
