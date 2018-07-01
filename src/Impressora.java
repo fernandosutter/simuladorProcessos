fadd50089ad115739adabbc747f9625936aff98c
Fernando Sutter <fernando.sutter1@gmail.com>
2018-06-30 23:24:30 -0300

    Codificado a parte inicial do Gerenciador de Processos.

--- src/Impressora.java
+++ src/Impressora.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
-
-public class Impressora extends Processo {
+public class Impressora {
 	public void main(String []args) {
-	System.out.println("Informe o quantum: ");
-	Scanner s = new Scanner (System.in);
-	quantum = s.nextInt();
+
 	}
  
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        Random randon = new Random();
+        Processo processo = new Processo();
+        System.out.println("Informe o Quantum:");
+        processo.quantum = scan.nextInt();
+        System.out.println("Informe o nome do novo Processo:");
+        processo.nomeProcesso = scan.next();
+        System.out.println("Informe a carga de trabalho:");
+        processo.cargadeTrabalho = scan.next();
+        processo.pid = randon.nextInt(50);
+
+        System.out.println("Processo " + processo.pid + " criado.");
+        System.out.println("Iniciando simulação de execução do processo...");
 
 
+
+
+
+        //System.out.println(processo.nomeProcesso);
+        //System.out.println(processo.quantum);
+        //System.out.println(processo.cargadeTrabalho);
+        //System.out.println(processo.pid);
     }
 }
